762ded1a7bce23320c92a3ab1399bef502d5973f
shubi <shubi-d@users.noreply.github.com>
2017-03-22 20:06:14 +0200

    bloopy

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     std::cout << "Hello, Mars!" << std::endl;
     for (int i=0; i<3; i++){
-        cout << "Fluffy!!" << endl;
+        cout << "bloopy!!" << endl;
     }
     return 0;
 }
